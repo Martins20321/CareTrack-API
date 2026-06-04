4532e429a111c295a9524f3196eab48632bff6b6
Martins20321 <jgmsilva11@gmail.com>
2026-06-04 20:23:18 -0300

    feat(infra): add DataSeeder to initialize default ADMIN user on startup

--- src/main/java/com/martinsdev/solicitation/api/repository/UserRepository.java
+++ src/main/java/com/martinsdev/solicitation/api/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.martinsdev.solicitation.api.repository;
 
 import com.martinsdev.solicitation.api.model.User;
+import com.martinsdev.solicitation.api.model.enums.RoleUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByRole(RoleUser role);
 }
