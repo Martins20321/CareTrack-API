7a9a1378e5897f29cd53759f21c1892e8f354e99
Martins20321 <jgmsilva11@gmail.com>
2026-06-06 19:33:41 -0300

    feat(dto): create ViaCepResponseDTO for integration with API ViaCEP

---
+++ src/main/java/com/martinsdev/solicitation/api/dto/ViaCepResponseDTO.java
@@ -0,0 +1,11 @@
+package com.martinsdev.solicitation.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record ViaCepResponseDTO(String cep,
+                                @JsonProperty("logradouro") String street,
+                                @JsonProperty("bairro") String neighborhood,
+                                @JsonProperty("localidade") String city,
+                                @JsonProperty("uf") String state,
+                                Boolean error) {
+}
