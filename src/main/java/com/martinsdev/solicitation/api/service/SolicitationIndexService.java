e0a42dacc2333dda659e57c5ddb6de9b5963ad86
Martins20321 <jgmsilva11@gmail.com>
2026-06-09 10:18:08 -0300

    feat(service): add SolicitationService for Elasticsearch indexing

---
+++ src/main/java/com/martinsdev/solicitation/api/service/SolicitationIndexService.java
@@ -0,0 +1,34 @@
+package com.martinsdev.solicitation.api.service;
+
+import com.martinsdev.solicitation.api.model.Solicitation;
+import com.martinsdev.solicitation.api.model.document.SolicitationDocument;
+import com.martinsdev.solicitation.api.repository.SolicitationSearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SolicitationIndexService {
+
+    private final SolicitationSearchRepository searchRepository;
+
+    public void index(Solicitation solicitation) {
+        SolicitationDocument document = SolicitationDocument.builder()
+                .id(String.valueOf(solicitation.getId()))
+                .clientId(String.valueOf(solicitation.getClient().getId()))
+                .status(solicitation.getStatus().name())
+                .createdAt(solicitation.getCreatedAt())
+                .submittedAt(solicitation.getSubmittedAt())
+                // Step 1 - Pode ser null
+                .serviceType(solicitation.getStepOneData() != null ? solicitation.getStepOneData().getServiceType().name() : null)
+                .title(solicitation.getStepOneData() != null ? solicitation.getStepOneData().getTitle() : null)
+                .description(solicitation.getStepOneData() != null ? solicitation.getStepOneData().getDescription() : null)
+                //Step 2 - Pode ser null
+                .state(solicitation.getStepTwoData() != null ? solicitation.getStepTwoData().getState() : null)
+                .city(solicitation.getStepTwoData() != null ? solicitation.getStepTwoData().getCity() : null)
+                //Step 3 - Pode ser null
+                .priority(solicitation.getStepThreeData() != null ? solicitation.getStepThreeData().getPriority().name() : null)
+                .build();
+        searchRepository.save(document);
+    }
+}
