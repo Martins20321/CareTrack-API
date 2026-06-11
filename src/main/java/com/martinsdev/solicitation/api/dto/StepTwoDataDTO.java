2b561fbbe613390dd444d444e39995d2229c316e
Martins20321 <jgmsilva11@gmail.com>
2026-06-11 18:20:48 -0300

    feat(dto): add all address fields to StepTwoDataDTO

--- src/main/java/com/martinsdev/solicitation/api/dto/StepTwoDataDTO.java
+++ src/main/java/com/martinsdev/solicitation/api/dto/StepTwoDataDTO.java
@@ -4,8 +4,12 @@ import com.martinsdev.solicitation.api.model.embedded.StepTwoData;
 
 public record StepTwoDataDTO(String cep,
                              String number,
-                             String complement) {
+                             String complement,
+                             String street,
+                             String neighborhood,
+                             String city,
+                             String state) {
     public StepTwoDataDTO(StepTwoData stepTwoData) {
-        this(stepTwoData.getCep(), stepTwoData.getNumber(), stepTwoData.getComplement());
+        this(stepTwoData.getCep(), stepTwoData.getNumber(), stepTwoData.getComplement(), stepTwoData.getStreet(), stepTwoData.getNeighborhood(), stepTwoData.getCity(), stepTwoData.getState());
     }
 }
