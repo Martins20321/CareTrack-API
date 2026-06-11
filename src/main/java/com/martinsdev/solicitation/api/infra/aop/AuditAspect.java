8899455b4948f40579df238e2c1b190e132d06fe
Martins20321 <jgmsilva11@gmail.com>
2026-06-11 18:26:54 -0300

    fix(aop): separate userEmail and userRole fields in AuditAspect and AuditLog

--- src/main/java/com/martinsdev/solicitation/api/infra/aop/AuditAspect.java
+++ src/main/java/com/martinsdev/solicitation/api/infra/aop/AuditAspect.java
@@ -31,20 +31,23 @@ public class AuditAspect {
         String action = audit.action().isBlank() ? methodName : audit.action();
 
         //Objeto autenticado
-        String userInfo = "anonymous";
+        String userEmail = "anonymous";
+        String userRole = "anonymous";
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof User user) {
-            userInfo = user.getEmail() + " [" + user.getRole() + "]";
+            userEmail = user.getEmail();
+            userRole = user.getRole().name();
         }
 
         try {
             Object result = joinPoint.proceed();
             long duration = System.currentTimeMillis() - start;
-            log.info("[AUDIT] action={} user={} duration={}ms status=SUCCESS", action, userInfo, duration);
+            log.info("[AUDIT] action={} user={} role={} duration={}ms status=SUCCESS", action, userEmail,userRole, duration);
 
             auditLogRepository.save(AuditLog.builder()
                             .action(action)
-                            .userEmail(userInfo)
+                            .userEmail(userEmail)
+                            .userRole(userRole)
                             .durationMs(duration)
                             .success(true)
                             .createdAt(LocalDateTime.now())
@@ -52,11 +55,12 @@ public class AuditAspect {
             return result;
         } catch (Throwable e) {
             long duration = System.currentTimeMillis() - start;
-            log.error("[AUDIT] action={} user={} duration={}ms status=ERROR", action, userInfo, duration, e.getMessage());
+            log.error("[AUDIT] action={} user={} role={} duration={}ms status=ERROR", action, userEmail, userRole, duration, e.getMessage());
 
             auditLogRepository.save(AuditLog.builder()
                     .action(action)
-                    .userEmail(userInfo)
+                    .userEmail(userEmail)
+                    .userRole(userRole)
                     .durationMs(duration)
                     .success(false)
                     .errorMessage(e.getMessage())
