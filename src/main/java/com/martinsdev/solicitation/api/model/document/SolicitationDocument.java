c5b05f9735486f602260d8cbf9e182a65203dafd
Martins20321 <jgmsilva11@gmail.com>
2026-06-12 12:00:34 -0300

    fix: correct erros in Elasticsearch

--- src/main/java/com/martinsdev/solicitation/api/model/document/SolicitationDocument.java
+++ src/main/java/com/martinsdev/solicitation/api/model/document/SolicitationDocument.java
@@ -1,7 +1,11 @@
 package com.martinsdev.solicitation.api.model.document;
 
+import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
 
@@ -13,15 +17,34 @@ import java.time.LocalDateTime;
 @Setter
 public class SolicitationDocument {
 
+    @Id
     private String id;
     private String clientId;
+
+    @Field(type = FieldType.Keyword)
     private String status;
+
+    @Field(type = FieldType.Keyword)
     private String serviceType;
+
+    @Field(type = FieldType.Text)
     private String title;
+
+    @Field(type = FieldType.Text)
     private String description;
+
+    @Field(type = FieldType.Keyword)
     private String state;
+
+    @Field(type = FieldType.Keyword)
     private String city;
+
+    @Field(type = FieldType.Keyword)
     private String priority;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
     private LocalDateTime createdAt;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
     private LocalDateTime submittedAt;
 }
